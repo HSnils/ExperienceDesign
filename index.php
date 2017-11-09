07f9ac24d4dfa34f6a3fcbd875740550c2bbbba9
Anders Martinsen <anders.adey@gmail.com>
2017-11-09 19:03:16 +0100

    finere piler

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ require_once('partials/header.php');
 								<td><b>'. $row['dayBooked'].'</b></td>
 								<td><b>'. $row['bookedFrom'].' - '.$row['bookedTo'].'</b>
                                 </td>
-                                <td><a href="delete.php?id='. $row['bookingID']. '" class="icon cancel"></a></td>
+                                <td><a href="delete.php?id='. $row['bookingID']. '" class="cancel">delete</a></td>
                                 <td><a href="room.php?id='. $row['roomName'].'" class="icon goTo"></a></td>
 							</tr>';
 							

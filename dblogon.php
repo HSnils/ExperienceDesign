ab21c8e128d490ecbf125ffbeba52fc6edacf5ab
Anders Martinsen <anders.adey@gmail.com>
2017-11-10 11:48:28 +0100

    go

--- dblogon.php
+++ dblogon.php
@@ -1,6 +1,12 @@
 <?php
-	$db_host = "mysql.stud.ntnu.no";
+	/*
+    $db_host = "mysql.stud.ntnu.no";
 	$db_database = "andma_xd3";
 	$db_user = "andma_demo";
 	$db_pass = "andma";
+    */
+    $db_host = "localhost";
+	$db_database = "xdesign_db";
+	$db_user = "root";
+	$db_pass = "";
 ?>

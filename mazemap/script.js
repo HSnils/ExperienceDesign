a6e669554b0fb1e71ec86a266fb3f75bdff0e9bd
matsaaj <MatsJacobsen@Matss-MacBook-Pro-2.local>
2017-11-10 21:27:35 +0100

    mazemap fixes

--- mazemap/script.js
+++ mazemap/script.js
@@ -1,4 +1,5 @@
 var mazeMarker;
+var result;
 
 var map = new Mazemap.Map({
   // container id specified in the HTML
@@ -21,7 +22,9 @@ var lnglatPos = {lng: 10.682424100019745, lat: 60.789850652563956};
 map.on('load', function() {
   // Get url parameters and highlight room
   var urlParams = new URLSearchParams(window.location.search);
+  var urlPath = window.location.pathname.split("/").pop();
 
+  // Get room id from URL and highlight
   if (urlParams.get('id')) {
     var query = urlParams.get('id');
     getRoom(query);
@@ -54,9 +57,61 @@ map.on('load', function() {
     lnglatPos = {lng: position.coords.longitude, lat: position.coords.latitude};
 
     blueDot.setLngLat(lnglatPos, {animate: true, duration: 300});
-    setTimeout(function() {
-      blueDot.addTo(map);
-    }, 300);
+    blueDot.addTo(map);
+
+    // Get available room list and populate roomDistance
+    if (urlPath == 'findroom.php') {
+      var deferred = $.Deferred();
+      var roomListCnt = $('tbody tr').length;
+
+      for (i = 0; i < roomListCnt; i++) {
+        var thisDistance;
+        var roomId = $('tbody tr .roomId')[i];
+        roomId = $(roomId).text();
+        var cnt = 0;
+
+        searchRoom.search(roomId).then( response => {
+          var poiId = response.results.features[0].properties.poiId;
+
+          Mazemap.Data.getPoi(poiId).then( poi => {
+            var roomLngLat = Mazemap.Util.getPoiLngLat(poi);
+            var thisDistance = posOverlap(roomLngLat, lnglatPos);
+            thisDistanceData = thisDistance.toFixed(4);
+            thisDistance = thisDistance.toFixed(0);
+            var roomDistance = $('tbody tr .roomDistance')[cnt];
+            var roomContainer = $('tbody tr')[cnt];
+
+            roomtempId = $('tbody tr .roomId')[cnt];
+            roomtempId = $(roomtempId).text();
+
+            $(roomDistance).html(thisDistance + 'm');
+            $(roomContainer).attr('distance', thisDistanceData);
+
+            cnt++;
+            if (cnt == i) {
+              deferred.resolve();
+            }
+          });
+        });
+      }
+      deferred.done(function() {
+        console.log("deff done");
+        jQuery.fn.sortDivs = function sortDivs() {
+          $("tr", this[0]).sort(dec_sort).appendTo(this[0]);
+          function dec_sort(a, b){ return ($(b).attr("distance")) > ($(a).attr("distance")) ? -1 : ($(b).attr("distance")) < ($(a).attr("distance")) ? 1 : -1; }
+        }
+        $('tbody').sortDivs();
+
+        /*
+        $('tbody tr').sort(function (a, b) {
+           var contentA =parseInt( $(a).attr('distance'));
+           var contentB =parseInt( $(b).attr('distance'));
+           console.log("a: " + contentA + " b: " + contentB);
+           return (contentA < contentB) ? -1 : (contentA > contentB) ? 1 : -1;
+        });
+        */
+      });
+    }
   }
 
   getLocation();
@@ -83,7 +138,7 @@ function onMapClick(e) {
   }).catch(function() {return false;});
 
 
-  posOverlap(lngLat, lnglatPos);
+  console.log(posOverlap(lngLat, lnglatPos));
 }
 
 function clearPoiMarker(poi) {
@@ -94,7 +149,7 @@ function clearPoiMarker(poi) {
   map.highlighter.clear();
 };
 
-var result;
+
 function posOverlap(a,b) {
   var x = (b.lat - a.lat);
   var y = (b.lng - a.lng);
@@ -110,9 +165,14 @@ function posOverlap(a,b) {
   var d = R * c;
   d = d*10;
 
+  var dist = d;
+  return dist;
+
+  /*
   if (d > 20) {
     console.log("Mer enn 20m fra rom");
   }
+  */
 }
 
 
@@ -177,7 +237,6 @@ function getRoom(query) {
     });
   });
 }
-
 
 // Add zoom and rotation controls to the map.
 //map.addControl(new Mazemap.mapboxgl.NavigationControl());
